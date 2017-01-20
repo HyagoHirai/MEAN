54741adabb9ce72a4f6135f6ef3dfffa6eff4e10
HyagoHirai <hyahirai@hotmail.com>
2017-01-20 14:36:29 -0200

    progress bar

--- app/app-component.js
+++ app/app-component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './cliente'], function(exports_1) {
+System.register(['angular2/core', './cliente', 'angular2/common', './progress.directive', './bar.component', './progressbar.component'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +8,7 @@ System.register(['angular2/core', './cliente'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, cliente_1;
+    var core_1, cliente_1, common_1, progress_directive_1, bar_component_1, progressbar_component_1;
     var AppComponent;
     return {
         setters:[
@@ -17,17 +17,33 @@ System.register(['angular2/core', './cliente'], function(exports_1) {
             },
             function (cliente_1_1) {
                 cliente_1 = cliente_1_1;
+            },
+            function (common_1_1) {
+                common_1 = common_1_1;
+            },
+            function (progress_directive_1_1) {
+                progress_directive_1 = progress_directive_1_1;
+            },
+            function (bar_component_1_1) {
+                bar_component_1 = bar_component_1_1;
+            },
+            function (progressbar_component_1_1) {
+                progressbar_component_1 = progressbar_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
                     this.tarefas = [];
+                    this.max = 200;
+                    this.stackedValues = [];
                     this.txtListagem = 'Listagem de Clientes';
                     this.clientes = [
                         new cliente_1.Cliente(1, "Alexandre"),
-                        new cliente_1.Cliente(2, "Samuel")
+                        new cliente_1.Cliente(2, "Samuel"),
                     ];
                     this.cliente = this.clientes[0];
+                    this.generateNewProgressValues();
+                    this.generateStackedValues();
                 }
                 AppComponent.prototype.selecionar = function (index) {
                     if (index < this.clientes.length) {
@@ -43,10 +59,46 @@ System.register(['angular2/core', './cliente'], function(exports_1) {
                 AppComponent.prototype.remover = function (index) {
                     this.tarefas.splice(index, 1);
                 };
+                AppComponent.prototype.generateNewProgressValues = function () {
+                    var value = Math.floor((Math.random() * 100) + 1);
+                    var type;
+                    if (value < 20) {
+                        type = 'success';
+                    }
+                    else if (value < 40) {
+                        type = 'info';
+                    }
+                    else if (value < 60) {
+                        type = 'warning';
+                    }
+                    else {
+                        type = 'danger';
+                    }
+                    this.currentValue = value;
+                    this.type = type;
+                };
+                ;
+                AppComponent.prototype.generateStackedValues = function () {
+                    var types = ['success', 'info', 'warning', 'danger'];
+                    this.stackedValues = [];
+                    var total = 0;
+                    for (var i = 0, n = Math.floor((Math.random() * 4) + 1); i < n; i++) {
+                        var index = Math.floor((Math.random() * 4));
+                        var value = Math.floor((Math.random() * 30) + 1);
+                        total += value;
+                        this.stackedValues.push({
+                            value: value,
+                            max: value,
+                            type: types[index]
+                        });
+                    }
+                };
+                ;
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'meu-app',
-                        template: "\n        <h1> Aula 2 </h1>\n        <h2> Listagem de Tarefas </h2>\n        <ul>\n            <li *ngFor=\"#tarefa of tarefas; #i = index\">\n                {{tarefa}} <a href=\"#\" (click)=\"remover(i)\">[X]</a>\n            </li>\n        </ul>\n        <input type=\"text\" name=\"tarefa\" placeholder=\"Digite a Tarefa\" [(ngModel)]=\"txtTarefa\">\n        <button (click)=\"adicionar()\">Adicionar</button>\n\n        <h1> Aula 3 </h1>\n        <h2> {{txtListagem}} </h2>\n        <p> Cliente selecionado: {{cliente.nome}} </p>\n        <ul>\n            <li *ngFor=\"#cli of clientes; #i = index\">\n                <a href=\"#\" (click)=\"selecionar(i)\">\n                    {{cli.id}} - {{cli.nome}}\n                </a>\n            </li>\n        </ul>\n    "
+                        template: "\n        <h3> Aula 2 </h3>\n        <h4> Listagem de Tarefas </h4>\n        <ul>\n            <li *ngFor=\"#tarefa of tarefas; #i = index\">\n                {{tarefa}} <a href=\"#\" (click)=\"remover(i)\">[X]</a>\n            </li>\n        </ul>\n        <input type=\"text\" name=\"tarefa\" placeholder=\"Digite a Tarefa\" [(ngModel)]=\"txtTarefa\">\n        <button (click)=\"adicionar()\">Adicionar</button>\n\n        <h3> Aula 3 </h3>\n        <h4> {{txtListagem}} </h4>\n        <p> Cliente selecionado: {{cliente.nome}} </p>\n        <ul>\n            <li *ngFor=\"#cli of clientes; #i = index\">\n                <a href=\"#\" (click)=\"selecionar(i)\">\n                    {{cli.id}} - {{cli.nome}}\n                </a>\n            </li>\n        </ul>\n\n        <h4>Angular 2 Progressbar Example</h4>\n        <progressbar class=\"progress-striped\" value=\"55\" type=\"warning\" max=\"200\">55%</progressbar>\n\n        <hr/>\n        <h4>Angular 2 Dynamic Progressbar Example</h4>\n        <button type=\"button\" class=\"btn btn-sm btn-default\" (click)=\"generateNewProgressValues()\">Generate New Values</button>\n\n        <progressbar [animate]=\"false\" [max]=\"max\" [value]=\"currentValue\">\n        <span style=\"color:white; white-space:nowrap;\">{{currentValue}} / {{max}}</span>\n        </progressbar>\n\n        <hr/>\n        <h4>Angular 2 Progressbar With Animation Example</h4>\n        <progressbar [animate]=\"true\" [value]=\"currentValue\" [type]=\"type\"><b>{{currentValue}}%</b></progressbar>\n        <hr/>\n    ",
+                        directives: [progress_directive_1.Progress, bar_component_1.Bar, progressbar_component_1.Progressbar, common_1.CORE_DIRECTIVES],
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app-component.ts
+++ app/app-component.ts
@@ -1,11 +1,15 @@
 import {Component} from 'angular2/core';
 import {Cliente} from './cliente';
+import { CORE_DIRECTIVES } from 'angular2/common';
+import {Progress} from './progress.directive';
+import {Bar} from './bar.component';
+import {Progressbar} from './progressbar.component';
 
 @Component({
     selector: 'meu-app',
     template: `
-        <h1> Aula 2 </h1>
-        <h2> Listagem de Tarefas </h2>
+        <h3> Aula 2 </h3>
+        <h4> Listagem de Tarefas </h4>
         <ul>
             <li *ngFor="#tarefa of tarefas; #i = index">
                 {{tarefa}} <a href="#" (click)="remover(i)">[X]</a>
@@ -14,8 +18,8 @@ import {Cliente} from './cliente';
         <input type="text" name="tarefa" placeholder="Digite a Tarefa" [(ngModel)]="txtTarefa">
         <button (click)="adicionar()">Adicionar</button>
 
-        <h1> Aula 3 </h1>
-        <h2> {{txtListagem}} </h2>
+        <h3> Aula 3 </h3>
+        <h4> {{txtListagem}} </h4>
         <p> Cliente selecionado: {{cliente.nome}} </p>
         <ul>
             <li *ngFor="#cli of clientes; #i = index">
@@ -24,7 +28,24 @@ import {Cliente} from './cliente';
                 </a>
             </li>
         </ul>
-    `
+
+        <h4>Angular 2 Progressbar Example</h4>
+        <progressbar class="progress-striped" value="55" type="warning" max="200">55%</progressbar>
+
+        <hr/>
+        <h4>Angular 2 Dynamic Progressbar Example</h4>
+        <button type="button" class="btn btn-sm btn-default" (click)="generateNewProgressValues()">Generate New Values</button>
+
+        <progressbar [animate]="false" [max]="max" [value]="currentValue">
+        <span style="color:white; white-space:nowrap;">{{currentValue}} / {{max}}</span>
+        </progressbar>
+
+        <hr/>
+        <h4>Angular 2 Progressbar With Animation Example</h4>
+        <progressbar [animate]="true" [value]="currentValue" [type]="type"><b>{{currentValue}}%</b></progressbar>
+        <hr/>
+    `,
+    directives: [Progress, Bar, Progressbar,CORE_DIRECTIVES],
 })
 export class AppComponent{
     public tarefas: string[] = [];
@@ -33,29 +54,73 @@ export class AppComponent{
     private clientes: Cliente[];
     private cliente: Cliente;
 
+    public max:number = 200;
+    public currentValue:number;
+    public type:string;
+    
+    public stackedValues:any[] = [];
+
     constructor(){
         this.txtListagem = 'Listagem de Clientes';
         this.clientes = [
-            new Cliente(1, "Alexandre");
-            new Cliente(2, "Samuel");
+            new Cliente(1, "Alexandre"),
+            new Cliente(2, "Samuel"),
         ]
         this.cliente = this.clientes[0];
+
+        this.generateNewProgressValues();
+        this.generateStackedValues();
     }
 
-    selecionar(index: number): void {
+
+    private selecionar(index: number): void {
         if(index < this.clientes.length) {
             this.cliente = this.clientes[index];
         }
     }
 
-    adicionar() {
+    private adicionar() {
         if(this.txtTarefa != '') {
             this.tarefas.push(this.txtTarefa);
             this.txtTarefa = '';
         }
     }
 
-    remover(index:number) {
+    private remover(index:number) {
         this.tarefas.splice(index,1);
     }
+
+    private generateNewProgressValues() {
+        let value = Math.floor((Math.random() * 100) + 1);
+        let type:string;
+
+        if (value < 20) {
+            type = 'success';
+        } else if (value < 40) {
+            type = 'info';
+        } else if (value < 60) {
+            type = 'warning';
+        } else {
+            type = 'danger';
+        }
+        this.currentValue = value;
+        this.type = type;
+    };
+
+    private generateStackedValues() {
+        let types = ['success', 'info', 'warning', 'danger'];
+
+        this.stackedValues = [];
+        let total = 0;
+        for (let i = 0, n = Math.floor((Math.random() * 4) + 1); i < n; i++) {
+            let index = Math.floor((Math.random() * 4));
+            let value = Math.floor((Math.random() * 30) + 1);
+            total += value;
+            this.stackedValues.push({
+                value: value,
+                max: value,
+                type: types[index]
+            });
+        }
+    };
 }
